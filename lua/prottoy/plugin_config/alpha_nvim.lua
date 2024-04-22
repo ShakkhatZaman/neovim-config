ec89de07c15b726ee16442afff45167656e5579f
Shakkhat Zaman <shakkhatzaman@gmail.com>
2024-04-22 22:15:43 +0600

    Reformatted all code

--- lua/prottoy/plugin_config/alpha_nvim.lua
+++ lua/prottoy/plugin_config/alpha_nvim.lua
@@ -39,32 +39,32 @@ dashboard.section.header.val = {
 }
 -- Set menu
 dashboard.section.buttons.val = {
-	dashboard.button( "e",  "  New file" , ":ene<CR>"),
-	dashboard.button( "f",  "  Find file", ":Telescope find_files<CR>"),
-	dashboard.button( "o",  "  Recent"   , ":Telescope oldfiles<CR>"),
-	dashboard.button( "s",  "  Settings" , ":e $MYVIMRC | :cd %:p:h | split . | wincmd k | pwd<CR>"),
-	dashboard.button( "q",  "󰗼  Quit NeoVIM", ":qa<CR>"),
-	dashboard.button( "t",  "  Terminal", ":ToggleTerm<CR>"),
-	dashboard.button( "F",  "󰙅  File tree", ":Neotree<CR>"),
+    dashboard.button( "e",  "  New file" , ":ene<CR>"),
+    dashboard.button( "f",  "  Find file", ":Telescope find_files<CR>"),
+    dashboard.button( "o",  "  Recent"   , ":Telescope oldfiles<CR>"),
+    dashboard.button( "s",  "  Settings" , ":e $MYVIMRC | :cd %:p:h | split . | wincmd k | pwd<CR>"),
+    dashboard.button( "q",  "󰗼  Quit NeoVIM", ":qa<CR>"),
+    dashboard.button( "t",  "  Terminal", ":ToggleTerm<CR>"),
+    dashboard.button( "F",  "󰙅  File tree", ":Neotree<CR>"),
 }
 
 local config = {
-	layout = {
-		{ type = "padding", val = 2 },
-		dashboard.section.header,
-		{ type = "padding", val = 2 },
-		dashboard.section.buttons,
-		dashboard.section.footer,
-	},
-	opts = {
-		margin = 5,
-	},
+    layout = {
+        { type = "padding", val = 2 },
+        dashboard.section.header,
+        { type = "padding", val = 2 },
+        dashboard.section.buttons,
+        dashboard.section.footer,
+    },
+    opts = {
+        margin = 5,
+    },
 }
 
 alpha.setup(config)
 
 -- Disable folding on alpha buffer
 vim.cmd([[
-	autocmd User AlphaReady set showtabline=0
+    autocmd User AlphaReady set showtabline=0
 ]])
 --autocmd FileType alpha setlocal nofoldenable
--- lua/prottoy/plugin_config/lsp_config.lua
+++ lua/prottoy/plugin_config/lsp_config.lua
@@ -1,50 +1,50 @@
 require("mason").setup({
-	ui = {
-		icons = {
-			package_installed = " ",
-			package_pending = "",
-			package_uninstalled = "◍"
-		}
-	}
+    ui = {
+        icons = {
+            package_installed = " ",
+            package_pending = "",
+            package_uninstalled = "◍"
+        }
+    }
 })
 
 require("mason-lspconfig").setup({
-	ensure_installed = {'lua_ls', "basedpyright", "clangd"},
+    ensure_installed = {'lua_ls', "basedpyright", "clangd"},
 })
 
 local lsp_on_attach = function(_, bufnr)
-	local opts = {noremap = true, silent = true, buffer = bufnr}
+    local opts = {noremap = true, silent = true, buffer = bufnr}
 
-	opts.desc = "Lsp rename"
-	vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
-	opts.desc = "Lsp hover info"
-	vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-	opts.desc = "Lsp type definition"
-	vim.keymap.set('n', 'D', vim.lsp.buf.type_definition, opts)
-	opts.desc = "Lsp code action"
-	vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, opts)
-	opts.desc = "Lsp references"
-	vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-	opts.desc = "Lsp implementations"
-	vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-	opts.desc = "Lsp declaration"
-	vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-	opts.desc = "Lsp definition"
-	vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+    opts.desc = "Lsp rename"
+    vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+    opts.desc = "Lsp hover info"
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+    opts.desc = "Lsp type definition"
+    vim.keymap.set('n', 'D', vim.lsp.buf.type_definition, opts)
+    opts.desc = "Lsp code action"
+    vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, opts)
+    opts.desc = "Lsp references"
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+    opts.desc = "Lsp implementations"
+    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+    opts.desc = "Lsp declaration"
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+    opts.desc = "Lsp definition"
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
     opts.desc = "Lsp format buffer"
     vim.keymap.set({ 'n', 'v' }, '<leader>lf', vim.lsp.buf.format, opts)
 
-	-- Telescope
-	opts.desc = "Telescope lsp type definition"
-	vim.keymap.set('n', 'TD', "<cmd>Telescope lsp_type_definitions<CR>", opts)
-	opts.desc = "Telescope lsp references"
-	vim.keymap.set('n', 'tr', "<cmd>Telescope lsp_references<CR>", opts)
-	opts.desc = "Telescope lsp implementations"
-	vim.keymap.set('n', 'ti', "<cmd>Telescope lsp_implementations<CR>", opts)
-	opts.desc = "Telescope lsp declaration"
-	vim.keymap.set('n', 'tD', "<cmd>Telescope lsp_declarations<CR>", opts)
-	opts.desc = "Telescope lsp definitions"
-	vim.keymap.set('n', 'td', "<cmd>Telescope lsp_definitions<CR>" , opts)
+    -- Telescope
+    opts.desc = "Telescope lsp type definition"
+    vim.keymap.set('n', 'TD', "<cmd>Telescope lsp_type_definitions<CR>", opts)
+    opts.desc = "Telescope lsp references"
+    vim.keymap.set('n', 'tr', "<cmd>Telescope lsp_references<CR>", opts)
+    opts.desc = "Telescope lsp implementations"
+    vim.keymap.set('n', 'ti', "<cmd>Telescope lsp_implementations<CR>", opts)
+    opts.desc = "Telescope lsp declaration"
+    vim.keymap.set('n', 'tD', "<cmd>Telescope lsp_declarations<CR>", opts)
+    opts.desc = "Telescope lsp definitions"
+    vim.keymap.set('n', 'td', "<cmd>Telescope lsp_definitions<CR>" , opts)
     opts.desc = 'Document Symbols'
     vim.keymap.set('n', '<leader>ds', "<cmd>Telescope lsp_document_symbols<CR>", opts)
     opts.desc = 'Workspace Symbols'
@@ -77,7 +77,7 @@ require("mason-lspconfig").setup_handlers({
 
 local signs = { Error = "󰅚 ", Warn = "󰀪 ", Hint = "󰌶", Info = " " }
 for type, icon in pairs(signs) do
-	local hl = "DiagnosticSign" .. type
-	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+    local hl = "DiagnosticSign" .. type
+    vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
 end
 
